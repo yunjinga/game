d927864e5a4c60c4cf779755c9b151084d8335ca
yunjinga <884443151@qq.com>
2022-03-10 23:48:00 +0800

    修改了部分寻路代码并且增加动画（未成功）

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -10,32 +10,74 @@ public class player : MonoBehaviour
     float rushTime = 5.0f;//可冲刺时间
     float waitTime = 1.0f;//等待时间
     public Slider slider;
-    public float speed = 10f;//速度
+    public float speed = 0f;//速度
     public float breath = 1.0f;
+    public float turnSpeed = 1.0f;
+    public float speed_a;
+    public float direct_y;//旋转角度
+    public double sin_1, cos_1;//相对于世界坐标的cos和sin
+    Animator ator;
+    Vector3 direct;
     void Start()
     {
         rd = GetComponent<Rigidbody>();
         slider.value = 1;
         slider.gameObject.SetActive(false);
+        speed_a = speed;
+        direct = transform.localEulerAngles;
+        if (direct.y > 180)
+        {
+            direct.y = direct.y - 360;
+        }
+        direct_y = direct.y;
+        //Debug.Log("direct_y=" + direct_y);
+        sin_1 = Mathf.Sin(Mathf.Deg2Rad * direct_y);
+        cos_1 = Mathf.Cos(Mathf.Deg2Rad * direct_y);
+        //Debug.Log("sin_1=" + sin_1 + " cos_1=" + cos_1);
+        ator = transform.GetComponent<Animator>();
     }
     void Update()
     {
+        Move();
+        RushTime();
+    }
+    
+    private void Move()
+    {
+        ator.SetBool("isRun", true);
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
         //Debug.Log(h);
-        Vector3 sped= new Vector3(h, 0, v);
+        Vector3 sped = new Vector3(h, 0, v);
+        Vector3 sped_x = new Vector3(h, 0, v);
         sped = sped.normalized;
-        rd.velocity = sped * speed;
+        //Debug.Log("sped.x * sin_1=" + sped.z * cos_1 + " sped.x * sin_1= " + sped.x * sin_1);
+        sped.x = (float)(sped_x.x * cos_1 + sped_x.z * sin_1);
+        sped.z = (float)(sped_x.z * cos_1 - sped_x.x * sin_1);
+        //sped.x = (float)(sped_x.x * cos_1 - sped_x.z * sin_1);
+        //sped.z = (float)(sped_x.z * cos_1 + sped_x.x * sin_1);
+        if(sped.x>0|| sped.z>0)
+        {
+            //ator.SetBool("isRun", true);
+        }
+        transform.Translate(sped * Time.deltaTime * speed, Space.World);
+        transform.LookAt(transform.position + sped);
+
+    }
+    private void RushTime()
+    {
         slider.value = rushTime / 5;
         if (Input.GetKey(KeyCode.LeftShift) && rushTime > 0)//冲刺计时
         {
             slider.gameObject.SetActive(true);
             waitTime = 2;
             rushTime -= Time.deltaTime;
-            rd.velocity *= 2;
+            if(speed==speed_a)
+            speed *= 2;
         }
         else
         {
+            speed = speed_a;
             if (waitTime > 0 && !Input.GetKey(KeyCode.LeftShift) && rushTime < 5)
             {
                 waitTime -= Time.deltaTime;
